23536c61fff23bad5b8419dadae0f61fc0d299ea
Ardan Maulana Nafi'a <143726800+maulanz83@users.noreply.github.com>
2024-06-02 21:42:19 +0700

    Update note_service.dart

--- Ardan/tugaspaardan/lib/services/note_service.dart
+++ Ardan/tugaspaardan/lib/services/note_service.dart
@@ -3,8 +3,12 @@ import 'package:http/http.dart' as http;
 import '../models/note.dart';
 
 class NoteService {
-  final String baseUrl = 'https://backend2-zxe5xu6lzq-et.a.run.app/notes';
-
+final String baseUrl = 'https://backend2-zxe5xu6lzq-et.a.run.app/notes';
+  //final String baseUrl = 'http://192.168.1.17:3000/notes';
+// dah ku mau lanjut tidur dl, nanti lanjut abis bangn isya, abis  maghrib mau belanja
+// btw ini dah bisa diliat peubahanya
+// mungkin bisa di coba dulu di flutternya
+// yg mu ganti diatas itu dah bisa ?
   Future<List<Note>> getNotes(String email) async {
     final response = await http.get(Uri.parse('$baseUrl/$email'));
 
@@ -27,6 +31,28 @@ class NoteService {
       return Note.fromJson(json.decode(response.body));
     } else {
       throw Exception('Failed to create note');
+    }
+  }
+
+  Future<Note> updateNote(int id, String title, String content) async {
+    final response = await http.put(
+      Uri.parse('$baseUrl/update/$id'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'title': title, 'content': content}),
+    );
+
+    if (response.statusCode == 200) {
+      return Note.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to update note');
+    }
+  }
+
+  Future<void> deleteNote(int id) async {
+    final response = await http.delete(Uri.parse('$baseUrl/delete/$id'));
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to delete note');
     }
   }
 }
